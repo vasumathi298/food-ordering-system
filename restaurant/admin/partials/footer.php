35021b662d475e8f753f778e0d5cbe42db32993f
vasumathi298 <72432533+vasumathi298@users.noreply.github.com>
2021-07-05 09:54:33 +0530

    Add files via upload

---
+++ restaurant/admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+			<div class="wrapper">
+				<p class="text-center"> 2021 All rights reserved, Garden of Eat’n Restaurant. Developed By -<a href="#"> Vasumathi Narayanan </a> </p>
+			</div>
+		</div>
+	</body>
+</html>
